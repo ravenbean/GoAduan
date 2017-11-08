4667414cf4ddc29762fe6a74a5628359f4e5821c
thaufan bean <taufanbean@hotmail.com>
2017-11-08 21:31:11 +0700

    nyamain punya yudha

---
+++ production/config/config.php
@@ -0,0 +1,5 @@
+<?php
+	function connect(){
+		return mysqli_connect("localhost","goaduanc_super","goaduan123","goaduanc_goaduandb");
+	}
+?>
--- production/source/get_aduan.php
+++ production/source/get_aduan.php
@@ -10,13 +10,19 @@
 	$Category = $_POST['Kategori'];
 
 	$query = "
-	INSERT INTO public_report (id_laporan, nama, tgl_aduan, detail_aduan, kategori, lokasi_gambar, DateInserted)
-	VALUES (null, '".$Name."', '".$Date."', '".$Detail."', '".$Category."', '', null)
-	";
-
+	SELECT id_laporan, nama, tgl_aduan, detail_aduan, kategori, lokasi_gambar, DateInserted FROM public_report";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) {
+		// output data of each row
+		while($row = mysqli_fetch_assoc($result)) {
+			echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+		}
+	} else {
+		echo "0 results";
+	}
 	//echo $query;
 
 	mysqli_query($connect,$query);
 
-	header('Location: ../pengaduan.php');
+	// header('Location: ../pengaduan.php');
 ?>
